e0ebaa52b2a5d3a94ac19edbe3428036674c8385
bryanagamk <bryanagamk@gmail.com>
2024-02-08 22:55:40 +0700

    feat: validation input

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,7 +1,12 @@
+import { IsEmail, IsNotEmpty } from "class-validator"
+
 export class CreateStudentDto {
     public id: number
+    @IsNotEmpty()
     public name: string
+    @IsNotEmpty()
     public course: string
+    @IsEmail()
     public email: string
     public phone: string
 }
