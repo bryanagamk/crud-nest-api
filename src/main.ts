4a6823cdf73a9b3532ca3a4671ba0b4b298eda24
bryanagamk <bryanagamk@gmail.com>
2024-02-09 22:35:24 +0700

    feat: format response

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors();
   await app.listen(3000);
 }
